452d028977f142050f62fe3ec9deb92e6dd2e39b
Alex Bigelow <alex.r.bigelow@gmail.com>
2020-03-05 08:48:07 -0700

    Commit dead file to fix git error

--- models/Filter.js
+++ models/Filter.js
@@ -1,7 +1,20 @@
-class Filter {
-  constructor ({ humanLabel, test }) {
+import IntrospectableMixin from '../utils/IntrospectableMixin.js';
+
+class Filter extends IntrospectableMixin(Object) {
+  constructor (humanLabel, spec) {
+    super();
     this.humanLabel = humanLabel;
-    this.test = test;
+    this.spec = spec;
+  }
+  test (transition) {
+    throw new Error('The test function should be overridden');
+  }
+  toString () {
+    return window.controller.jsonCodec({
+      humanLabel: this.humanLabel,
+      type: this.type,
+      spec: this.spec
+    });
   }
 }
 
