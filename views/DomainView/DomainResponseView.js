3fdbbce07ef6007187a97566794e5ada8b28a5ba
Alex Bigelow <alex.r.bigelow@gmail.com>
2020-03-08 15:01:45 -0700

    Clean up duplicate submissions, flag a couple more junk responses, and clarify some of the vis text

--- views/DomainView/DomainResponseView.js
+++ views/DomainView/DomainResponseView.js
@@ -1,3 +1,4 @@
+/* globals d3 */
 import VisView from '../VisView/VisView.js';
 
 class DomainResponseView extends VisView {
@@ -12,6 +13,14 @@ class DomainResponseView extends VisView {
   setup () {
     this.d3el.html(this.resources[1]);
     super.setup();
+
+    const datasetLabelField = d3.select(this.d3el.select('[data-key="datasetLabel"]')
+      .node().parentNode);
+    datasetLabelField.append('div')
+      .classed('errata', true)
+      .text(`Note that this input field displayed as a select menu on some
+devices, and the suggested dataset labels may have appeared as a limited-choice
+dropdown for some participants.`);
   }
 }
 export default DomainResponseView;
